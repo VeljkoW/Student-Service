6e844af65b170ea769bf86e4d329a5debe042249
Vojnic <vojnicnemanja@gmail.com>
2023-11-05 15:39:23 +0100

    ?

---
+++ CLI/models/Adress.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+    public class Adress: ISerializable
+    {
+        public string Street { get; set; }
+        public int StreetNumber{ get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string[] ToCSV() {
+            string[] sb = {
+                Street,
+                StreetNumber.ToString(),City, State
+            };
+            return sb;
+        }
+
+        public void FromCSV(string[] values) {
+            Street= values[0];
+            StreetNumber= int.Parse(values[1]);
+            City= values[2];
+            State= values[3];
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Street: "+Street+", ");
+            sb.Append("Street number:"+StreetNumber.ToString()+", ");
+            sb.Append("City: "+City+", ");
+            sb.Append("State: "+State+", ");
+            return sb.ToString();
+        }
+        public Adress(string a,int b,string c,string d)
+        {
+            Street = a;
+            StreetNumber= b;
+            City= c;
+            State = d;
+        }
+        public Adress()
+        {
+            Street = "Bulevar Oslobodjenja";
+            StreetNumber = 12;
+            City = "Novi Sad";
+            State = "Serbia";
+        }
+    }
+}
---
+++ CLI/models/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+    public class Student : ISerializable
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? BirthDate { get; set; }
+        public Adress? StudentAdress { get; set; }
+        public int? Phone { get; set; }
+        public string? Email { get; set; }
+        public int IndexNumber { get; set; }
+        public int StudentYear { get; set; }
+        public float GradeAvg { get; set; }
+        public int[]? FinishedExams { get; set; }
+        public List<string>? ToDoExams { get; set; }
+
+        public string[] ToCSV()
+        {
+            string[] retString = { Name, Surname, BirthDate, StudentAdress.ToString(), Phone.ToString(), Email, IndexNumber.ToString(), StudentYear.ToString(), GradeAvg.ToString(), FinishedExams.ToString(), ToDoExams.ToString() };
+            return retString;
+        }
+        void FromCSV(string[] values)
+        {
+            Name = values[0];
+            Surname = values[1];
+            BirthDate = values[2];
+            StudentAdress = new Adress(values[3], int.Parse(values[4]), values[5], values[6]);
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder x = new StringBuilder();
+            x.Append("Name: " + Name + ", ");
+            x.Append("Surname: " + Surname + ", ");
+            x.Append("Surname: " + Surname + ", ");
+            x.Append("BirthDate: " + BirthDate + ", ");
+            x.Append("Adress: " + StudentAdress.ToString() + ", ");
+            return x.ToString();
+        }
+    }
+}
