2e643aa6b55c105ba243d2fb60c84fcf3bd90bd8
Vojnic <vojnicnemanja@gmail.com>
2023-11-08 12:33:48 +0100

    Added DAO and ConsoleView and changed a few models

---
+++ CLI/ConsoleView/ConsoleViewUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+    static class ConsoleViewUtils
+    {
+        public static int SafeInputInt()
+        {
+            int input;
+            string rawInput = System.Console.ReadLine() ?? string.Empty;
+            while (!int.TryParse(rawInput, out input))
+            {
+                System.Console.WriteLine("Not a valid number, try again: ");
+
+                rawInput = System.Console.ReadLine() ?? string.Empty;
+            }
+            return input;
+        }
+        public static DateOnly SafeInputDate()
+        {
+            DateOnly input;
+
+            string rawInput = System.Console.ReadLine() ?? string.Empty;
+
+            while (!DateOnly.TryParse(rawInput, out input))
+            {
+                System.Console.WriteLine("Not a valid date, try again: ");
+
+                rawInput = System.Console.ReadLine() ?? string.Empty;
+            }
+
+            return input;
+        }
+    }
+}
---
+++ CLI/DAO/AdressDAO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+    public class AdressDAO
+    {
+        private readonly List<Adress> Adresses;
+        private readonly Storage<Adress> storage;
+        public AdressDAO()
+        {
+            storage = new Storage<Adress>("Adresses.txt");
+            Adresses = storage.Load();
+        }
+        private int GenerateId()
+        {
+            if (Adresses.Count == 0) return 0;
+            return Adresses[^1].Id + 1;
+        }
+        public Adress AddAdress(Adress adresa)
+        {
+            adresa.Id = GenerateId();
+            Adresses.Add(adresa);
+            storage.Save(Adresses);
+            return adresa;
+        }
+        public Adress? UpdateAdress(Adress adress)
+        {
+            Adress? old = GetAdressById(adress.Id);
+            if (old is null) return null;
+            old.StreetNumber = adress.StreetNumber;
+            old.Street= adress.Street;
+            old.State = adress.State;
+            old.City = adress.City;
+            storage.Save(Adresses);
+            return old;
+        }
+        public Adress? RemoveAdress(int id)
+        {
+            Adress? vehicle = GetAdressById(id);
+            if (vehicle == null) return null;
+
+            Adresses.Remove(vehicle);
+            storage.Save(Adresses);
+            return vehicle;
+        }
+        private Adress? GetAdressById(int id)
+        {
+            return Adresses.Find(v => v.Id == id);
+        }
+        public List<Adress> GetAllVehicles()
+        {
+            return Adresses;
+        }
+    }
+}
